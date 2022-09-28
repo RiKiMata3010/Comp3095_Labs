b1e50080ee62e80c56f074d2a932d44ab9e9149b
Phoenix Armand Ani <PhoenixArmand30@outlook.com>
2022-09-27 20:23:50 -0400

    Commit & Push

---
+++ Comp3095_Labs/src/test/java/ca/gbc/comp3095/comp3095_labs/Comp3095LabsApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.gbc.comp3095.comp3095_labs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Comp3095LabsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
